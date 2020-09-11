4f14f4a4736c857784300f195bcae78c0414557e
Luis Coelho <luis.fs.coelho@pm.me>
2020-09-11 07:51:15 -0300

    add error handling

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 import '@shared/infra/typeorm';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 const app = express();
@@ -9,6 +10,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('server started on port 3333!');
